77a7ec433a2e2fa48917fbe6c68fcbdd24e96c74
Philip Johnston <phijofigre@gmail.com>
2025-03-28 17:11:35 +0000

    Changes to make bullets kill enemies work

--- enemies/ball.gd
+++ enemies/ball.gd
@@ -1,8 +1,10 @@
-extends StaticBody2D
+extends AnimatableBody2D
 
-var direction = Vector2(-1, 0)
+var direction: Vector2
 var speed = 5
 var velocity: Vector2
+var rebounding = false
+@onready var timer = %TimeoutTimer
 
 func _ready() -> void:
 	velocity = direction * speed
@@ -17,13 +19,21 @@ func _physics_process(delta: float) -> void:
 			queue_free()
 
 func rebound(angle: float):
-	print("angle", rad_to_deg(angle))
+	speed = 10
+	timer.wait_time = 10.0
+	timer.start()
+	rebounding = true
 	angle = deg_to_rad(rad_to_deg(angle) - 90)
 	direction = Vector2.from_angle(angle)
 	velocity = direction * speed
-	print(direction)
 
 func rebound_other():
+	#self.collision_mask = 1
+	#self.collision_layer = 1
+	speed = 10
+	timer.wait_time = 10.0
+	timer.start()
+	rebounding = true
 	var mouse_pos = self.get_global_mouse_position()
 	var angle_to = rad_to_deg(mouse_pos.angle_to_point(global_position)) - 180
 	direction = Vector2.from_angle(deg_to_rad(angle_to))
--- enemies/ball_emitter.gd
+++ enemies/ball_emitter.gd
@@ -10,4 +10,11 @@ func _process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	var new_ball = ball_object.instantiate()
+	new_ball.direction = Vector2(-1,0)
 	add_child(new_ball)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("rebound"):
+		if body.rebounding == true:
+			queue_free()
